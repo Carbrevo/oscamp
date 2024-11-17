b0e51b5f5f59583984779a41578977ed2ca20456
Pablo <pablo@carbrevo@hotmail.com>
2024-11-17 23:26:04 +0800

    001

--- arceos/labs/lab_allocator/src/lib.rs
+++ arceos/labs/lab_allocator/src/lib.rs
@@ -3,41 +3,70 @@
 #![no_std]
 #![allow(unused_variables)]
 
-use allocator::{BaseAllocator, ByteAllocator, AllocResult};
+#[macro_use]
+extern crate axlog;
+
+use allocator::{BaseAllocator, ByteAllocator, AllocResult, AllocError};
 use core::ptr::NonNull;
 use core::alloc::Layout;
 
-pub struct LabByteAllocator;
+
+pub struct LabByteAllocator {
+    start: usize,
+    size: usize,
+    free: usize,
+}
 
 impl LabByteAllocator {
     pub const fn new() -> Self {
-        Self
+        Self {
+            start: 0,
+            size: 0,
+            free: 0,
+        }
     }
 }
 
 impl BaseAllocator for LabByteAllocator {
     fn init(&mut self, start: usize, size: usize) {
-        unimplemented!();
+        //unimplemented!();
+        ax_println!("init_memory: start={:#x}, size={}", start, size);
     }
     fn add_memory(&mut self, start: usize, size: usize) -> AllocResult {
-        unimplemented!();
+        //unimplemented!();
+        ax_println!("add_memory: start={:#x}, size={}", start, size);
+        //unimplemented!();
+        self.start = start;
+        self.size = size;
+        self.free = 0;
+        Ok(())
     }
 }
 
 impl ByteAllocator for LabByteAllocator {
     fn alloc(&mut self, layout: Layout) -> AllocResult<NonNull<u8>> {
-        unimplemented!();
+        //unimplemented!();
+        if self.size < layout.size() {
+            return Err(AllocError::NoMemory);
+        }
+
+        let alloc_addr = (self.start + self.free) as *mut u8;
+        self.free += layout.size();
+        Ok(NonNull::new(alloc_addr).unwrap())
     }
     fn dealloc(&mut self, pos: NonNull<u8>, layout: Layout) {
-        unimplemented!();
+        //unimplemented!();
     }
     fn total_bytes(&self) -> usize {
-        unimplemented!();
+        //unimplemented!();
+        self.size
     }
     fn used_bytes(&self) -> usize {
-        unimplemented!();
+        //unimplemented!();
+        self.free
     }
     fn available_bytes(&self) -> usize {
-        unimplemented!();
+        //unimplemented!();
+        self.size - self.free
     }
 }
